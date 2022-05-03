03f7fe4aaef9e61875e88b614ca72cdc36a20d3c
Vokivon <vokivon.p@gmail.com>
2022-05-03 09:59:12 +0300

    Создал проект HW_04

---
+++ HW_theme_04/TaskSwitch.cs
@@ -0,0 +1,44 @@
+namespace HW_theme_04;
+
+public class BaseSwitch
+{
+    static void Main(string[] Args)
+    {
+        int userChoice;
+        
+        Console.WriteLine("Доступны следующие номера задач: 1; 2; 3 ");
+        Console.WriteLine("Для выхода введите любой другой символ");
+        Console.Write("Введите номер задачи для исполнения:");
+        string inputValue = Console.ReadLine();
+        
+        Action<string[]> executor = CloseProg; // делегат для исполнения
+        
+        if (int.TryParse(inputValue, out userChoice))
+        {
+            switch (userChoice)
+            {
+                case 1 : Console.WriteLine($"Будет выполнена задача {userChoice}");
+                    executor = Task_01.Start;
+                    break;
+                // case 2 : Console.WriteLine($"Будет выполнена задача {userChoice}");
+                //     executor = Task_02.Start;
+                //     break;
+                // case 3 : Console.WriteLine($"Будет выполнена задача {userChoice}");
+                //     executor = Task_03.Start;
+                //     break;
+            }
+        }
+        
+        executor(Args);
+       
+    }
+    
+    static void CloseProg(string[] Args)
+    {
+        Console.WriteLine("Некорректный ввод номера задачи");
+        Console.WriteLine("Завершение программы");
+        Console.WriteLine("Нажмите любую клавишу...");
+        Console.ReadKey();
+    }
+
+}
