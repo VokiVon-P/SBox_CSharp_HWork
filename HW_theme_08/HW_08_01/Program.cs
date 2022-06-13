fd5809f3d3240e1815464c682e9dd7b93086efad
Vokivon <vokivon.p@gmail.com>
2022-06-13 13:19:48 +0300

    Закончил Task 01

--- HW_theme_08/HW_08_01/Program.cs
+++ HW_theme_08/HW_08_01/Program.cs
@@ -1,40 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Что нужно сделать
+    Создайте лист целых чисел. 
+    Заполните лист 100 случайными числами в диапазоне от 0 до 100. 
+    Выведите коллекцию чисел на экран. 
+    Удалите из листа числа, которые больше 25, но меньше 50. 
+    Снова выведите числа на экран. 
+ */
 
-using System.Linq.Expressions;
 
-Console.WriteLine("Hello, World!");
-
-Random rnd = new Random();
-List<int> mainList = new List<int>();
-
+// заполнение списка значениями от 0 до 100
 void FillList(List<int> list)
 {
+    Random rnd = new Random();
     for (int i = 0; i < 100; i++)
     {
-        mainList.Add(rnd.Next(0, 100));
+        list.Add(rnd.Next(0, 100));
     }
-    Console.WriteLine(list.Count);
 }
 
+// печать списка в консоль
 void PrintList(List<int> list)
 {
     for (int i = 0; i < list.Count; i++)
     {
         if (i % 25 == 0) Console.WriteLine();
-        Console.Write($"{list[i]} ");
+        Console.Write($"{list[i], 2} ");
     }
     Console.WriteLine();
+    Console.WriteLine($"В списке {list.Count} элементов.");
 }
 
+// фильтр списка по условию
 void FilterList(List<int> list)
 {
     // инлайн условие отбора
     bool FilterCondition(int x) => x is > 25 and < 50; 
-    
     list.RemoveAll(FilterCondition);
-    Console.WriteLine(list.Count);
+    
+    Console.WriteLine();
+    Console.WriteLine("Список отфильтрован!");
 }
 
+// -------------------------------------
+// Код исполнения
+
+Console.WriteLine("Задание 1. Работа с листом ");
+
+List<int> mainList = new List<int>();
 FillList(mainList);
 PrintList(mainList);
 FilterList(mainList);
